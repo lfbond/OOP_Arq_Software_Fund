49a5a2c530313c464ef940f718f9a5bb81ca8c96
Luis Felipe Bond <luisfelipe.bond@gmail.com>
2025-09-12 16:22:21 -0300

    primeiro commit fundamentos

---
+++ OOP/Pilares_OOP/heranca.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace OOP
+{
+    public class Funcionario : Pessoa
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            var Funcionario = new Funcionario()
+            {
+                Nome = "João da Silva",
+                DataNascimento = Convert.ToDateTime(value: "1990/01/02"),
+                DataAdmissao = DateTime.Now,
+                Registro = "0123456",
+            };
+        }
+    }
+}
